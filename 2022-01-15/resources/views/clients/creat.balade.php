c146e8659948b161854eb28f5a1948e20cde7b0b
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2022-01-15 10:31:46 +0200

    Update creat.balade.php

---
+++ 2022-01-15/resources/views/clients/creat.balade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Add client</title>
+</head>
+<body>
+
+<div class="container">
+    <h1>Add Client form</h1>
+
+<form class="form-control mh-2" action="{{ {{route('client.store') }}" method="POST">
+
+<input class="form-control" name="client_name" type="text" placeholder="Client name">
+
+<input class="form-control" name="client_surename" type="text" placeholder="Client surename">
+
+<input class="form-control" name="client_username" type="text" placeholder="User name">
+
+<select id="client_company" class="form-control" disabled name="client_company" size="1">
+    <option value="null" selected="selected">Select company</option>
+</select>
+
+
+@csrf
+<input class="btn btn-primary" type="submit" value="Add">
+<a class="btn btn-secondary" href="{{{{route('client.index') }}">Back to list</a>
+
+</form>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
