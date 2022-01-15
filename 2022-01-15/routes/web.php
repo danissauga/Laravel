8ac6a5655bf91922db224209f628246856555819
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2022-01-15 09:50:25 +0200

    Update web.php

--- 2022-01-15/routes/web.php
+++ 2022-01-15/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
 Route::prefix('clients')->group(function() {
    
     Route::get('', 'App\Http\Controllers\ClientController@index')->name('client.index');
