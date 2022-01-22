c8d2158b3ddd6a3987e38c9a0119a4d326334f53
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2022-01-22 12:42:14 +0200

    Add Some updates to table creation and other places

--- 2022-01-22/resources/views/studets/index.blade.php
+++ 2022-01-22/resources/views/studets/index.blade.php
@@ -5,16 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>companys list</title>
+    <title>Students list</title>
 </head>
 <body>
 
 <div class="container">
-    <h1>Companys list</h1>
+    <h1>Students list</h1>
     <div class="form-group pt-2 pb-2">
-        <a class="btn btn-primary" href="{{route('company.create') }}">Add company</a>
-        <a class="btn btn-primary" href="{{route('type.create') }}">Add company type</a>
-        <a class="btn btn-secondary" href="{{route('client.index') }}">Companies list</a>
+        <a class="btn btn-primary" href="{{route('student.create') }}">Add student</a>
+        <a class="btn btn-primary" href="{{route('school.create') }}">Add School </a>
+        <a class="btn btn-secondary" href="{{route('school.index') }}">School list</a>
 </div>
  
         @if (session()->has('error_message'))
@@ -38,9 +38,8 @@
             <tr>
                 <th>ID</th>
                 <th>Name</th>
-                <th>Company type</th>
                 <th>Descriotion</th>
-                <th>Clients count</th>
+                <th>Difficulty</th>
                 <th class="col-2" colspan="3">Tools</th>
             </tr>
 
