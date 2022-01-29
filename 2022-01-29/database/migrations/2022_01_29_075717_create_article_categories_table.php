55b6b943b313b80a50440f92940a77139aa81434
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2022-01-29 10:25:45 +0200

    Add 2022-01-29 lesson source

---
+++ 2022-01-29/database/migrations/2022_01_29_075717_create_article_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->unsignedBigInteger('image_id');
+            $table->foreign('image_id')->references('id')->on('article_images');  
+            $table->unsignedBigInteger('article_id');
+            $table->foreign('article_id')->references('id')->on('articles');  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_categories');
+    }
+}
