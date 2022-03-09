6d89834e42bd9680f87d9115926d5c76868145b4
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2022-03-09 23:03:38 +0200

    *Full article CRUD ad some Type fix

--- 2022-03-05/app/Http/Controllers/ArticleController.php
+++ 2022-03-05/app/Http/Controllers/ArticleController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Http\Requests\StoreArticleRequest;
 use App\Http\Requests\UpdateArticleRequest;
+use App\Models\Type;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
@@ -15,7 +17,9 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Article::all();
+        $types = Type::all();
+        return view('article.index',['articles'=>$articles, 'types'=>$types]);  
     }
 
     /**
@@ -38,7 +42,26 @@ class ArticleController extends Controller
     {
         //
     }
+    public function storeAjax(Request $request)
+    {
+        $article = new Article;
+        $article->title = $request->article_title;
+        $article->type_id = $request->article_type_id;
+        $article->description = $request->article_description;
 
+        $article->save();
+
+        $article_array = array(
+            'successMessage' => "Article stored succesfuly",
+            'articleId' => $article->id,
+            'articleTypeId' => $article->type_id,
+            'articleTitle' => $article->title,
+            'articleDescription' => $article->description,
+        );
+
+        $json_response =response()->json($article_array); 
+        return $json_response;
+    }
     /**
      * Display the specified resource.
      *
@@ -49,7 +72,26 @@ class ArticleController extends Controller
     {
         //
     }
-
+  
+ /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function showAjax(Article $article)
+    {   
+        $article_array = array(
+                'successMessage' => "Article show succesfuly",
+                'articleId' => $article->id,
+                'articleTypeTitle' => $article->articleHasType->title,
+                'articleTypeId' => $article->type_id,
+                'articleTitle' => $article->title,
+                'articleDescription' => $article->description,
+        );
+        $json_response =response()->json($article_array);
+        return $json_response;
+    }
     /**
      * Show the form for editing the specified resource.
      *
@@ -72,7 +114,31 @@ class ArticleController extends Controller
     {
         //
     }
+/**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTypeRequest  $request
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function updateAjax(Request $request, Article $article)
+    {
+        $article->title = $request->article_title;
+        $article->description = $request->article_description;
+        $article->type_id = $request->article_type_id;
+        $article->save();
 
+        $article_array = array(
+            'successMessage' => "Article". $article->title ." updated succesfuly",
+            'articleId' => $article->id,
+            'articleTypeId' => $article->type_id,
+            'articleTypeTitle' => $article->articleHasType->title,
+            'articleTitle' => $article->title,
+            'articleDescription' => $article->description,
+        );
+        $json_response = response()->json($article_array);
+        return $json_response;
+    }
     /**
      * Remove the specified resource from storage.
      *
