d70c382da84ccca13ed01f485f719feb38d88a2b
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2022-03-19 15:36:53 +0200

    client side index, store function.

---
+++ 2022-03-19/client-side/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::prefix('images')->group(function() {
+//     Route::get('', 'App\Http\Controllers\ImageController@index')->name('image.index');
+//     Route::get('create', 'App\Http\Controllers\ClientController@create')->name('client.create');
+//     Route::post('store', 'App\Http\Controllers\ClientController@store')->name('client.store');
+//     Route::get('edit/{id}', 'App\Http\Controllers\ClientController@edit')->name('client.edit');
+//     Route::post('update/{id}', 'App\Http\Controllers\ClientController@update')->name('client.update');
+//     Route::get('show/{client}', 'App\Http\Controllers\ClientController@show')->name('client.show');
+//     Route::post('delete/{id}', 'App\Http\Controllers\ClientController@destroy')->name('client.delete');
+// });
+Route::get('/', function () {
+    return view('image.index');   
+});
