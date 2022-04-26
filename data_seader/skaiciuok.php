3209e7b0da7fd47f7e681b0e85277b59cc538356
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2022-04-26 21:39:15 +0300

    Some updates and news

---
+++ data_seader/skaiciuok.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = array(1,2,4,7,1,6,2,8); //duomenys
+$ats=array();
+$kiek=0;
+$sum=array();
+$minval=10; //parinktas pagal tai, kad skaiciai visi yra iki 10 - mties
+//$ii=count($a);
+
+
+function remove_used_elements($mass) {
+    
+}
+
+for ($i=0; $i<count($a); $i++) {
+   if ($a[$i] = $minval) {
+
+   }
+
+}
